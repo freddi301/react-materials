21ba12770e59fe2cdd9fb769aa4cd4c24b8e5e5a
Frederik Batuna <gobi301@gmail.com>
2023-06-22 12:21:09 +0200

    fix

--- liferay/create-react-app-adapted/README.md
+++ liferay/create-react-app-adapted/README.md
@@ -1,6 +1,6 @@
 # create-react-app liferay:adapt
 
-- `yarn create react-app --tyemplate typescript my-app`
+- `yarn create react-app --template typescript my-app`
 - `yo liferay-js:adapt` https://help.liferay.com/hc/en-us/articles/360035467712-Adapting-Existing-Apps-to-Run-on-Liferay-DXP
 - `docker-compose up` see [docker compose file](./docker-compose.yaml)
 - `yarn build:liferay`
--- liferay/create-react-app-adapted/src/App.tsx
+++ liferay/create-react-app-adapted/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
-import { LIFERAY_PARAMS } from './configuration';
+import React from "react";
+import "./App.css";
+import { LIFERAY_PARAMS } from "./configuration";
 
 function App() {
   return (
-      <pre className='configuration-debug'>{JSON.stringify(LIFERAY_PARAMS, null, 2)}</pre>
+    <pre className="configuration-debug">
+      {JSON.stringify(LIFERAY_PARAMS, null, 2)}
+    </pre>
   );
 }
 

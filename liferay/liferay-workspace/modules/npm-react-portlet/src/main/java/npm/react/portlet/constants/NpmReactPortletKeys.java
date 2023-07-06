e75195a36476aa765236e0fafcbf39712f62c144
Frederik Batuna <gobi301@gmail.com>
2023-07-06 10:19:28 +0200

    add: npm-react-portlet

---
+++ liferay/liferay-workspace/modules/npm-react-portlet/src/main/java/npm/react/portlet/constants/NpmReactPortletKeys.java
@@ -0,0 +1,10 @@
+package npm.react.portlet.constants;
+
+/**
+ * @author Frederik
+ */
+public class NpmReactPortletKeys {
+
+	public static final String NpmReact = "npmreact";
+
+}
---
+++ liferay/liferay-workspace/modules/npm-react-portlet/src/main/java/npm/react/portlet/portlet/NpmReactPortlet.java
@@ -0,0 +1,52 @@
+package npm.react.portlet.portlet;
+
+import npm.react.portlet.constants.NpmReactPortletKeys;
+
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Frederik
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/index.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + NpmReactPortletKeys.NpmReact,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class NpmReactPortlet extends MVCPortlet {
+
+	@Override
+	public void doView(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		renderRequest.setAttribute(
+			"mainRequire",
+			_npmResolver.resolveModuleName("npm-react-portlet") + " as main");
+
+		super.doView(renderRequest, renderResponse);
+	}
+
+	@Reference
+	private NPMResolver _npmResolver;
+
+}
